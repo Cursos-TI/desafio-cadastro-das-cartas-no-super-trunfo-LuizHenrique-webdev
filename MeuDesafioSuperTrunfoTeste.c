4d896589f6569098a51bc513e0f0f90c43b5d113
LuizHenrique-webdev <almeidaluiz578@gmail.com>
2025-03-26 17:31:39 -0400

    Atualizar o MeuDesafioSuperTrunfoTeste.c
    alterei meu teste no githubmobile

--- MeuDesafioSuperTrunfoTeste.c
+++ MeuDesafioSuperTrunfoTeste.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
 int main(){
-     
+     printf ("Informe uma letra que represente um estado" );
+     printf ("informe o nome da Cidade: ");
+     printf ("informe a populacao: ");
+     printf ("informe a area: ");
+     printf ("informe o PIB: ");
+     printf ("informe a quantidade de pontos turisticos: ");
 
    return 0;
 }
